5983eeb16b615c5712153f8e0875bdb7816a96ed
hsyn <deneme147146@gmail.com>
2024-03-19 15:16:48 +0300

    productservice created and tried

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import ProductService from './services/productService';
 
 function App() {
+
+  const [products, setProducts]=useState([]) 
+
+  useEffect(()=>{
+    let products = new ProductService()
+    products.getProducts().then(response=> setProducts(response.data))
+  },[])
+
+  console.log(ProductService)
   return (
     <div className="App">
+
+      {
+        products.map((product)=>{
+         return <p key={product.id}>{product.name}/ {product.id}</p>
+        })
+      }
       
     </div>
   );
