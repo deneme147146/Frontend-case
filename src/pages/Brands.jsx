e6374bb544eabd941d923eb10a741e17588a0fa3
hsyn <deneme147146@gmail.com>
2024-03-20 12:36:25 +0300

    model and brands completed

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import '../style/Brands.css'
 
 const Brands = () => {
   return (
     <div>
       <p style={{color:'grey'}}>Brands</p>
+      <div className="brands-container">
+        <div className="brands-card">
+        <input className='input-search'
+        type='text'
+        placeholder='Search'
+        />
+          <div>
+            <input type="checkbox" id="Apple" name="brands" value="Apple" />
+            <label htmlFor="Apple">Apple</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="Samsung" name="brands" value="Samsung" />
+            <label htmlFor="Samsung">Samsung</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="Huawei" name="brands" value="Huawei" />
+            <label htmlFor="Huawei">Huawei</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="Xiaomi" name="brands" value="Xiaomi" />
+            <label htmlFor="Xiaomi">Xiaomi</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="faefa" name="brands" value="faefa" />
+            <label htmlFor="faefa">faefa</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="efaffga" name="brands" value="efaffga" />
+            <label htmlFor="efaffga">efaffga</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="rjrjrjr" name="brands" value="rjrjrjr" />
+            <label htmlFor="rjrjrjr">rjrjrjr</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="ğpkfek" name="brands" value="ğpkfek" />
+            <label htmlFor="ğpkfek">ğpkfek</label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Model.jsx
+++ src/pages/Model.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import '../style/Model.css'
 
 const Model = () => {
   return (
-    <div>Model</div>
+    <div>
+      <p style={{color:'grey'}}>Model</p>
+      <div className="model-container">
+        <div className="model-card">
+        <input className='input-search-model'
+        type='text'
+        placeholder='Search'
+        />
+          <div>
+            <input type="checkbox" id="11" name="model" value="11" />
+            <label htmlFor="11">11</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="12Pro" name="model" value="12Pro" />
+            <label htmlFor="12Pro">12Pro</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="13Pro" name="model" value="13Pro" />
+            <label htmlFor="13Pro">13Pro</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="14" name="model" value="14" />
+            <label htmlFor="14">14</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="14Pro" name="model" value="14Pro" />
+            <label htmlFor="14Pro">14Pro</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="15" name="model" value="15" />
+            <label htmlFor="15">15</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="11" name="model" value="11" />
+            <label htmlFor="11">11</label>
+          </div>
+          <br />
+          <div>
+            <input type="checkbox" id="15Pro" name="model" value="15Pro" />
+            <label htmlFor="15Pro">15Pro</label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
